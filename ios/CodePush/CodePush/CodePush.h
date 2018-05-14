6acb628c8ab7b1698a9a92a7ba26e7c1e6be59a1
Chris Moulds <chmoulds@microsoft.com>
2018-05-14 08:13:53 -0700

    Initial project commit

---
+++ ios/CodePush/CodePush/CodePush.h
@@ -0,0 +1,18 @@
+//
+//  CodePush.h
+//  CodePush
+//
+//  Copyright © 2018 MSFT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CodePush.
+FOUNDATION_EXPORT double CodePushVersionNumber;
+
+//! Project version string for CodePush.
+FOUNDATION_EXPORT const unsigned char CodePushVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CodePush/PublicHeader.h>
+
+
